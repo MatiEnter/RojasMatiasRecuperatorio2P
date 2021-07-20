459bf2d97ec8d74c0e86157dd8587bb31c5ab9bf
MatiEnter <79776808+MatiEnter@users.noreply.github.com>
2021-07-20 20:40:37 -0300

    Delete PBII2021P1ApellidoNombre directory

--- PBII2021P1ApellidoNombre/src/ar/edu/unlam/pb2/parcial1/Enumeradores/Calificacion.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlam.pb2.parcial1.Enumeradores;
-
-public enum Calificacion {
-
-	ATP,
-	MAYORES_SIETE,
-	MAYORES_DIECIOCHO,
-}
--- PBII2021P1ApellidoNombre/src/ar/edu/unlam/pb2/parcial1/Productos/Comestible.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlam.pb2.parcial1.Productos;
-
-public class Comestible extends Producto{
-
-	public Comestible(Integer cODIGO_ESPERADO, String dESCRIPCION_ESPERADA) {
-	}
-}
